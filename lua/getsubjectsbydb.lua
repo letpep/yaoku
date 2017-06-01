f88a874146bb055d5475d84c3b9adec15ffa729b
leexin <cnbdlixin@163.com>
2017-06-01 10:14:35 +0800

    从数据库获取数据脚本

---
+++ lua/getsubjectsbydb.lua
@@ -0,0 +1,16 @@
+# import module
+	local json = require "cjson"
+
+    local res = ngx.location.capture('/postgres',
+        { args = {sql = "SELECT * FROM yaoku_subject" } }
+    )
+
+    local status = res.status
+    local body = json.decode(res.body)
+
+    if status == 200 then
+        status = true
+    else
+        status = false
+    end
+    ngx.say(body)
