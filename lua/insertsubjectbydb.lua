a6e5ed8bd4798ea5b9ffb195a974b8ea8d3f1b90
leexin <cnbdlixin@163.com>
2017-06-01 10:23:48 +0800

    插入数据库

---
+++ lua/insertsubjectbydb.lua
@@ -0,0 +1,16 @@
+# import module
+	local json = require "cjson"
+
+    local res = ngx.location.capture('/postgres',
+        { args = {sql = "insert into yaoku_subject(subject,url) values('测试数据tt','http:22')" } }
+    )
+
+    local status = res.status
+    local body = json.decode(res.body)
+
+    if status == 200 then
+        status = true
+    else
+        status = false
+    end
+    ngx.say(status)
