25f907243cf9cf70e8436fc5a28e31162d76e424
leexin <cnbdlixin@163.com>
2017-06-01 18:12:34 +0800

    分页搜索

---
+++ lua/searchList.lua
@@ -0,0 +1,38 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: lee_xin
+-- Date: 17/6/1
+-- Time: 下午6:01
+-- To 支持分页搜索
+--
+local json = require("cjson")
+local request_method = ngx.var.request_method
+local totalnum = 0
+local pageno = 1
+local pagecount =5
+local pagestart = 0
+local findkey =""
+if "GET" == request_method then
+    local rdskey =nil
+    args  = ngx.req.get_uri_args()
+    for key,val in pairs(args) do
+        if "findkey" == key  then
+            findkey = val
+        elseif "pageno" == key then
+            pageno = val
+        elseif "pagecount" == key then
+            pagecount = val
+        end
+        end
+end
+
+pagestart = pageno*pagecount-1
+local res = ngx.location.capture('/postgres',
+    { args = {sql = "SELECT count(1) FROM yaoku_subject where subject like '%"..findkey.."%';" } }
+)
+
+local status = res.status
+local body = json.decode(res.body)
+ngx.say(body)
+
+
