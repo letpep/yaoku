7ece0bcbc5f2e82716a0f76f088abff7ee36d1ac
李信 <li.xin@sf-express.com>
2017-07-24 11:40:03 +0800

    字符串正则4

--- lua/util/stringmatch.lua
+++ lua/util/stringmatch.lua
@@ -5,13 +5,15 @@
 -- Time: 上午10:59
 -- To change this template use File | Settings | File Templates.
 --
-local sgmatch = string.gmatch
+local sgmatch = string.gmatch --返回迭代器
+local smatch = string.match --返回具体串
 local stringv = "134abc445ABC@245"
 local result =''
 for v in sgmatch(stringv,'%d') do
     result =  result..v
 end
 
-local k,v1 =  sgmatch(stringv,'(%d)abc(%d)')
-
-ngx.say(k..v1)
+local k1,v1 =  smatch(stringv,'(%d*)%a*(%d*)')
+result = result..k1
+result = result..v1
+ngx.say(result)
