8d2bb19787a42fa060a4f21fba9118f1b9175c81
李信 <li.xin@sf-express.com>
2017-07-24 11:09:47 +0800

    字符串正则

---
+++ lua/util/stringmatch.lua
@@ -0,0 +1,14 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: lee_xin
+-- Date: 17/7/24
+-- Time: 上午10:59
+-- To change this template use File | Settings | File Templates.
+--
+local sgmatch = string.gmatch
+local stringv = "134abc445ABC@245"
+local result =''
+for v in sgmatch(stringv,'%d') do
+    result =  result..v
+end
+ngx.say(result)
